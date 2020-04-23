aafbd0afb16e57440e6ec4fe44eee9511fe3e6fc
pranaygupta36 <pranayguptastudent@gmail.com>
2020-04-24 02:34:03 +0530

    added facenet for detection

--- get_face_bb.py
+++ get_face_bb.py
@@ -1,32 +1,27 @@
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+import numpy as np
 import cv2
-import sys
 
-def get_face_bb():
-    ''' get the boubding box of the face
-        detects a face, saves the face as a png file, return true
-        returns false if no face face is found'''
-    faceCascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+def get_face_bb(detection_thresh):
+    ''' Uses facenet to detect face and then an inception net to calculate the embedding of the detected face'''
+    device = 'cpu'
+    mtcnn = MTCNN(
+        image_size=160, margin=0, min_face_size=20,
+        thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+        device=device
+    )
+    resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
     video_capture = cv2.VideoCapture(0)
-    flag = 0
     while True:
         ret, frame = video_capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor = 1.1,
-            minNeighbors = 5,
-            minSize = (100, 100),
-            flags = cv2.CASCADE_SCALE_IMAGE
-        )
-        if len(faces) > 0:
-            x, y, w, h = faces[0]
-            cv2.imwrite('detected_face.png', frame[y-50:y+h+50, x-50: x+w+50])
-            flag = 1
-            break
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        print('camera started')
+        x_aligned, prob = mtcnn(frame, return_prob=True)
+        if x_aligned is not None:
+            print('Face detected with probability: {:8f}'.format(prob))
+            if prob > detection_thresh:
+                break
+            
+    face_embedding = resnet(torch.stack([x_aligned]))
 
-    if flag:
-        return True
-    else:
-        return False
+    return face_embedding
