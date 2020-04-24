fe6b60c3af7352706e50cd611f331fe94f4ee625
pranaygupta36 <pranayguptastudent@gmail.com>
2020-04-24 22:43:34 +0530

    face recognition on server/client, multithreading done

--- client/client.py
+++ client/client.py
@@ -1,14 +1,16 @@
+from get_face_vector import get_face_vector
 import socket
 import numpy
 import pickle
 
 class Client():
-
+    '''Client class for individual cameras'''
     def __init__(self, server_ip, server_port):
         self.sock = socket.socket()
         self.sock.connect((server_ip, server_port))
     
     def send_msg(self, msg):
+        '''Sends a text msg to the server'''
         try:
             self.sock.send(msg.encode())
             return "Message Sent"
@@ -16,26 +18,44 @@ class Client():
             return "Error occurred in sending message"
 
     def recv_msg(self):
+        '''receives a text msg from the server'''
         try:
             return self.sock.recv(1024).decode()
         except:
             return "Error occurred in receiving message"
+
+    def send_vector(self, data):
+        '''sends a vector to the server'''
+        try: 
+            serialized_data = pickle.dumps(data, protocol=2)
+            self.sock.sendall(serialized_data)
+            return "Vector Sent"
+        except:
+            return "Error occurred in sending the vector"
+    
+    def close_connection(self):
+        '''ends the connection with the server'''
+        try:
+            self.sock.close()
+            return "Connection Closed"
+        except:
+            return "Error occurred in closing connection" 
 
 
 if __name__ == '__main__':
     client = Client("127.0.0.1", 8000)
     out = client.send_msg("Request to connect")
     msg = client.recv_msg()
-    print(msg)    
-
-# data= numpy.ones((1, 60))
-
-# serialized_data = pickle.dumps(data, protocol=2)
-# sock.sendall(serialized_data)
-# sock.close()
-# sock = socket.socket()
-# data= numpy.zeros((1, 60))
-# sock.connect(('localhost',8000))
-# serialized_data = pickle.dumps(data, protocol=2)
-# sock.sendall(serialized_data)
-# sock.close()
+    print("server -> " + msg)
+    while True:
+        inp = input("Ready? Y or N : ")
+        out = client.send_msg(inp)
+        if inp == "Y":
+            face_vec = get_face_vector(.95)
+            out = client.send_vector(face_vec)
+            msg = client.recv_msg()
+            print("server -> " + msg)
+        else:
+            print("Finished")
+            out = client.close_connection()
+            break
--- server/socket_server.py
+++ server/socket_server.py
@@ -1,7 +1,9 @@
-import sys
+from recognize_face import recognize_face
 import socket
 import numpy
 import pickle
+import select
+import _thread
 
 class Server:
     '''Main server class for handling and managing records'''
@@ -31,15 +33,57 @@ class Server:
         except:
             return "Error occurred in receiving message"
 
+    def recv_vector(self, csock):
+        '''Receives face vector from the client'''
+        try:
+            block = csock.recv(1024)
+            data = b'' + block
+            while len(block) == 1024:
+                block = csock.recv(1024)
+                data += block
+        
+            return pickle.loads(data, encoding='bytes')
+        except:
+            return "Error occurred in receiving the vector"
 
+    def close_connection(self, csock, caddr):
+        '''Close connection with the client'''
+        try:
+            csock.close()
+            return "Connection Closed with " + str(caddr)
+        except:
+            return "Error occurred in closing connection"
+
+
+def start_procedure(server, csock, caddr):
+    '''Function for starting the procedure to mark attendance'''
+    msg = server.recv_msg(csock)
+    print(str(caddr) + " -> "+ msg)
+    out = server.send_msg(csock, "Connection complete")
+    print(out)
+    while True:
+        out = server.recv_msg(csock)
+        if out == "Y":
+            face_vec = server.recv_vector(csock)
+            if isinstance(face_vec, str):
+                out = server.send_msg(csock, face_vec)
+            else: 
+                roll_number = recognize_face(face_vec, 100)
+                if roll_number is not None:
+                    out = server.send_msg(csock, roll_number + " marked present")
+                else:
+                    out = server.send_msg(csock, "Face not recognized")
+        else: 
+            out = server.close_connection(csock, caddr)
+            print(out)
+            break
+    return None
+        
 
 if __name__ == '__main__':
     server = Server("127.0.0.1", 8000)
     while True:
         csock, caddr = server.accept_connection()
-        msg = server.recv_msg(csock)
-        print(msg)
-        out = server.send_msg(csock, "Connection complete")
-
+        _thread.start_new_thread(start_procedure,(server, csock, caddr))
         
         
